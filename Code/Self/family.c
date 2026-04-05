e9459adc1696d7d2f3e007895074916b7463b012
Aakarsh Maurya <mauryaaakarsh11@gmail.com>
2026-04-06 00:24:00 +0530

    Completed family.c

--- Code/Self/family.c
+++ Code/Self/family.c
@@ -5,23 +5,20 @@ Print both to the trace pipe: "PID 1234 was spawned by PID 1000".
 
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
-#include <bpf/bpf_tracing.h>
+#include <bpf/bpf_core_read.h>
+
+#define MASK 0xFFFFFFFF
 
 SEC("tracepoint/syscalls/sys_enter_execve")
 int get_process_parent_pid(void* ctx){
-    u32 pid = bpf_get_current_pid_tgid() & 0xFFFFFFFF;
+    pid_t pid = bpf_get_current_pid_tgid() & MASK;
     struct task_struct *task = (void *)bpf_get_current_task();
     struct task_struct *parent; 
 
-    int err;
+    pid_t p_pid = BPF_CORE_READ(task, parent, tgid) & MASK;
+    bpf_printk("PID %d was spawned by %d", pid, p_pid);
 
-    err = bpf_core_read(&p, sizeof(void *), &task->parent);
-    if (err){
-        bpf_printk("Cannot load parent for %d", pid);
-        return 0;
-
-    }
-    bpf_printk("PID %d was spawned by %d", pid);
+    //bpf_printk("PID %d was spawned", pid);
     return 0;
 }
 
---
+++ README.md
@@ -0,0 +1,1 @@
+For libbpf/C use clang for compilation and bpftool for autoattaching view using trace_pipe
